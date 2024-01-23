d0329f4e7014b556fab9c9678ca1a9468bf842d5
yubnee31 <tydbqls@gmail.com>
2024-01-23 16:40:07 +0900

    fix: 충돌수정

--- src/api/currentUser.ts
+++ src/api/currentUser.ts
@@ -5,11 +5,7 @@ const getCurrentUser = async () => {
   const {
     data: {user},
   } = await supabase.auth.getUser();
-<<<<<<< HEAD
   console.log('현재 로그인 된 유저', user?.id);
-=======
-  console.log('현재 로그인 된 유저', user!.id);
->>>>>>> c419a7b9944b8aa7cc57113968e692f46125798e
   console.log('user', user);
   return user;
 };
